0df500a6c33a473c07c74cd33abdbb950bdad015
derek-carter_fmgl <derek.carter@fortescue.com>
2025-05-09 15:59:38 +0800

    more work

--- main.go
+++ main.go
@@ -1,47 +1,33 @@
 package main
 
-import (
-	"anvil/cli"
-	"errors"
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-)
+import "anvil/config"
 
-func ensureDir(dirName string) error {
-	err := os.Mkdir(dirName, os.ModeDir)
-	if err == nil {
-		return nil
-	}
-	if os.IsExist(err) {
-		// check that the existing path is a directory
-		info, err := os.Stat(dirName)
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			return errors.New("path exists but is not a directory")
-		}
-		return nil
-	}
-	return err
+type ConfigFile struct {
+	UserName string `json:"UserName"`
+	BaseUrl  string `json:"BaseUrl"`
 }
 
 func main() {
-	// get home dir
-	dirname, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//get home dir
+	// dirname, err := os.UserHomeDir()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// configpath := filepath.Join(dirname, ".anvil-cli", "config.json")
+	// // create anvil-cli dir in homedir
+	// newpath := filepath.Join(dirname, ".anvil-cli")
+	// // err = os.MkdirAll(newpath, os.ModePerm)
+	// if err := ensureDir(newpath); err != nil {
+	// 	fmt.Println("Directory creation failed with error: " + err.Error())
+	// 	os.Exit(1)
+	// }
+	// fmt.Println(dirname)
+	// os.Exit(cli.CLI())
+	// Create an interactive text input with a mask for password input
 
-	// create anvil-cli dir in homedir
-	newpath := filepath.Join(dirname, ".anvil-cli")
-	// err = os.MkdirAll(newpath, os.ModePerm)
-	if err := ensureDir(newpath); err != nil {
-		fmt.Println("Directory creation failed with error: " + err.Error())
-		os.Exit(1)
-	}
-	fmt.Println(dirname)
-	os.Exit(cli.CLI())
+	config.CreateConfigOrOpen()
+	// passwordInput := pterm.DefaultInteractiveTextInput.WithMask("*")
+	// result, _ := passwordInput.Show("Enter your password")
+	// logger := pterm.DefaultLogger
+	// logger.Info("Password received", logger.Args("password", result))
 }
